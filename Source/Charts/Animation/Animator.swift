81eb9e8c1e8429af2f558a7411321837b484f22f
harshshah <harsh.shah@agileinfoways.com>
2018-01-25 17:59:08 +0530

    no message

--- Source/Charts/Animation/Animator.swift
+++ Source/Charts/Animation/Animator.swift
@@ -2,7 +2,7 @@
 //  Animator.swift
 //  Charts
 //
-//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//   Cohen Gindi & Philipp Jahoda
 //  A port of MPAndroidChart for iOS
 //  Licensed under Apache License 2.0
 //

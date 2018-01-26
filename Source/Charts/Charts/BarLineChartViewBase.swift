aa7c1b081f5e97e4a58bcbd6c58f51bb5640f1e1
harshshah <harsh.shah@agileinfoways.com>
2018-01-26 19:20:58 +0530

    no message

--- Source/Charts/Charts/BarLineChartViewBase.swift
+++ Source/Charts/Charts/BarLineChartViewBase.swift
@@ -3,8 +3,6 @@
 //  Charts
 //
 //  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
-//  A port of MPAndroidChart for iOS
-//  Licensed under Apache License 2.0
 //
 //  https://github.com/danielgindi/Charts
 //
